3fc48d2917d89a92c06233466c350790f8ce2b01
wanglili <lilyfun42@163.com>
2021-06-28 17:03:34 +0800

    项目初始化完成

---
+++ components/musichead/musichead.vue
@@ -0,0 +1,57 @@
+<template>
+	<view class="music-head">
+		<view class="music-head-icon" v-if="icon">
+			<text class="iconfont icon-zuojiantou" @tap="handleToBack"></text>|<text class="iconfont icon-shouye" @tap="handleToHome"></text>
+		</view>
+		<view>
+			{{title}}
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"musichead",
+		props:['title','icon'],
+		data() {
+			return {
+				
+			};
+		},
+		methods:{
+			handleToBack(){
+				uni.navigateBack();
+			},
+			handleToHome(){
+				uni.navigateTo({
+					url: '/pages/index/index',
+				});
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.music-head{
+		width:100%;
+		height: 75px;
+		font-size: 16px;
+		line-height: 80px;
+		text-align: center;
+		color:black;
+		position:relative;
+	}
+	.music-head-icon{
+		position:absolute;
+		left:8px;
+		top:28px;
+		width:90px;
+		height:31px;
+		background:rgba(0,0,0,0.4);
+		color:white;
+		line-height:31px;
+		border-radius: 15px;
+		display: flex;
+		justify-content: space-evenly;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,55 @@
+<template>
+	<view class="index">
+		<musichead title="网易云音乐" :icon="false"></musichead>
+		<view class="container">
+			<scroll-view scroll-y="true" >
+				<view class="index-search">
+					<text class="iconfont icon-fangdajing"></text>
+					<input type="text" placeholder="搜索歌曲" />
+				</view>
+			</scroll-view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import '@/common/iconfont.css';
+	import musichead from "@/components/musichead/musichead.vue"
+	export default {
+		components:{
+			musichead
+		},
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style scoped>
+	.index{}
+	.index-search{
+		display: flex;
+		align-items: center;
+		height: 70rpx;
+		margin:70rpx 30rpx 30rpx 30rpx;
+		background:#f7f7f7;
+		border-radius: 50rpx;
+	}
+	.index-search text{
+		font-size:26rpx;
+		margin-left: 28rpx;
+		margin-right: 26rpx;
+	}
+	.index-search input{
+		font-size: 28rpx;
+		flex:1;
+	}
+</style>
